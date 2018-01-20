1e16fcb9a92a9c14ad974e7714d3c65fd0285c7a
Paul Tiller <pftiller@gmail.com>
2018-01-20 15:07:27 -0600

    Setting up server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static('/server/public'));
+
+app.listen(port,()=>{
+    console.log('up on', port);
+});
