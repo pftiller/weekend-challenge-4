608d02ef1cc8fb3f1335d22b9108f3390a006785
Paul Tiller <pftiller@gmail.com>
2018-01-20 17:50:02 -0600

    Updates to package.json and server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const router = require('./routes/photos.router');
 const port = process.env.PORT || 5000;
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.json()); 
 
 //routes
-
+app.use('/photos', router);
 
 
 
